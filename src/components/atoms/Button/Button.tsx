b9f704ef3d53adb6378a3ecccdbd2dc00d3864e8
Chris <menkhus.chris@gmail.com>
2022-03-23 21:15:31 -0600

    #1 Restrcuctured Button styling to better fit with more applications

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,5 +1,4 @@
 import makeStyles from '@utils/makeStyles'
-import classNames from 'classnames'
 import { forwardRef } from 'react'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
@@ -10,38 +9,32 @@ type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
-    {
-      type = 'button',
-      className = '',
-      size = 'md',
-      variant = 'primary',
-      icon,
-      ...props
-    },
+    { variant = 'primary', size = 'md', icon, className = '', ...props },
     ref
   ) => {
-    const Icon = icon
+    const IconElement = () => {
+      if (icon) {
+        const Icon = icon
+        return <Icon className={styles.icon} />
+      } else return <></>
+    }
+
     return (
       <button
+        {...props}
         ref={ref}
-        type={type}
-        className={classNames([
+        type={props.type}
+        disabled={props.disabled}
+        className={makeStyles([
           styles.container,
           variants[variant],
           sizes[size],
-          className,
+          'disabled:bg-gray-400 disabled:bg-gradient-to-r disabled:from-gray-400 disabled:to-gray-400 disabled:dark:bg-gray-400 disabled:dark:bg-gradient-to-r  disabled:dark:from-gray-400  disabled:dark:to-gray-400',
+          className || '',
         ])}
-        {...props}
       >
         <span className={styles.text}>{props.children}</span>
-        {Icon && (
-          <Icon
-            className={makeStyles([
-              styles.icon.main,
-              size === sizes.lg && styles.icon.large,
-            ])}
-          />
-        )}
+        <IconElement />
       </button>
     )
   }
@@ -49,27 +42,23 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 
 const styles = {
   container:
-    'flex justify-center items-center rounded border text-black bg-white dark:text-white dark:bg-dark  hover:scale-105 transition-all border-0',
+    'flex justify-center items-center rounded border text-black bg-white dark:text-white dark:bg-black hover:scale-105 transition-all border-0',
   text: 'mx-1',
-  icon: {
-    main: 'h-4 w-4 mx-1 my-auto',
-    large: 'h-14 w-16',
-  },
+  icon: 'h-4 w-4 mx-1 my-auto',
 }
 
 const variants = {
   primary:
-    'bg-gradient-to-r from-primary to-secondary text-white shadow-xl shadow-secondary/30',
-  secondary: 'border-2 border-secondary bg-white shadaw-lg',
-  inverse: 'border-dark',
-  borderless: 'border-none shadow-none bg-transparent dark:bg-transparent',
+    'bg-gradient-to-r text-white dark:text-white shadow-xl shadow-fuchsia-800/20 from-rose-500 to-orange-400',
+  secondary:
+    'bg-gray-800 dark:bg-gray-700 text-white dark:text-white shadow-xl disabled:bg-gray-400 disabled:dark:bg-gray-400',
 }
 
 const sizes = {
   sm: 'py-2 px-4 text-sm',
-  md: 'py-2 px-6 text-md',
-  lg: 'py-3 px-8 text-lg',
-  xl: 'py-4 px-10 text-lg',
+  md: 'py-2 px-6 text-base',
+  lg: 'py-2 px-10 text-lg',
+  xl: 'py-3 px-10 text-xl',
 }
 
 Button.displayName = 'ButtonComponent'
---
+++ src/components/atoms/Button/ButtonOld.tsx
@@ -0,0 +1,75 @@
+import makeStyles from '@utils/makeStyles'
+import classNames from 'classnames'
+import { forwardRef } from 'react'
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: keyof typeof variants
+  size?: keyof typeof sizes
+  icon?: React.ElementType
+}
+
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  (
+    {
+      type = 'button',
+      className = '',
+      size = 'md',
+      variant = 'primary',
+      icon,
+      ...props
+    },
+    ref
+  ) => {
+    const Icon = icon
+    return (
+      <button
+        ref={ref}
+        type={type}
+        className={classNames([
+          styles.container,
+          variants[variant],
+          sizes[size],
+          className,
+        ])}
+        {...props}
+      >
+        <span className={styles.text}>{props.children}</span>
+        {Icon && (
+          <Icon
+            className={makeStyles([
+              styles.icon.main,
+              size === sizes.lg && styles.icon.large,
+            ])}
+          />
+        )}
+      </button>
+    )
+  }
+)
+
+const styles = {
+  container:
+    'flex justify-center items-center rounded border text-black bg-white dark:text-white dark:bg-dark  hover:scale-105 transition-all border-0',
+  text: 'mx-1',
+  icon: {
+    main: 'h-4 w-4 mx-1 my-auto',
+    large: 'h-14 w-16',
+  },
+}
+
+const variants = {
+  primary:
+    'bg-gradient-to-r from-primary to-secondary text-white shadow-xl shadow-secondary/30',
+  secondary: 'border-2 border-secondary bg-white shadaw-lg',
+  inverse: 'border-dark',
+  borderless: 'border-none shadow-none bg-transparent dark:bg-transparent',
+}
+
+const sizes = {
+  sm: 'py-2 px-4 text-sm',
+  md: 'py-2 px-6 text-md',
+  lg: 'py-3 px-8 text-lg',
+  xl: 'py-4 px-10 text-lg',
+}
+
+Button.displayName = 'ButtonComponent'
